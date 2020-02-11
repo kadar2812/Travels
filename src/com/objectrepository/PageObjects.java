dde76f3fe3695f528b546ea182aab4fcdcb38712
kadar2812 <abdulkadar2812@gmail.com>
2020-02-11 08:53:46 +0530

    added new package

---
+++ src/com/objectrepository/PageObjects.java
@@ -0,0 +1,15 @@
+package com.objectrepository;
+
+public class PageObjects {
+	
+	public void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void test2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
