da0e999d39cec930cf5fcc8163104fee4f00647a
kadar2812 <abdulkadar2812@gmail.com>
2020-02-11 08:27:35 +0530

    new project

---
+++ src/com/resources/TicketBooking.java
@@ -0,0 +1,15 @@
+package com.resources;
+
+public class TicketBooking {
+	
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
