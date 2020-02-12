1b8beda97a0c10c249a30d7ec2e0adeefe7dbfe4
Vinith Kumar <vinithv97@gmail.com>
2020-02-12 10:10:43 +0530

    important class added

---
+++ src/com/objectrepository/Important.java
@@ -0,0 +1,18 @@
+package com.objectrepository;
+
+public class Important {
+	public void boardingPoint() {
+	// TODO Auto-generated method stub
+
+}
+	public void droppingPoint() {
+		// TODO Auto-generated method stub
+
+	}public void date() {
+		// TODO Auto-generated method stub
+
+	}public void time() {
+		// TODO Auto-generated method stub
+
+	}
+}
